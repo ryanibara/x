fb49be10432c09d1ac9840ddae9a3b4cc2c95dce
ryanibara <santana.rey0202@gmail.com>
2024-03-12 06:37:19 -0400

    update

---
+++ 1.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch -f --env-filter '
+
+OLD_EMAIL="ahmedhindawi2001@gmail.com"
+CORRECT_NAME="ryanibara"
+CORRECT_EMAIL="santana.rey0202@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
